e5125c7e6a2f16b0a405955a37c31504d498c031
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-04-01 17:49:16 +0100

    update
    Quase a concluir o ecrã de login, icon do olho no sitio falta só fazer o efeito de mostrar e "incriptar" a pass

--- plataforma-mobile/mobile/lib/loginpage.dart
+++ plataforma-mobile/mobile/lib/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +27,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String text = '';
+  bool isPasswordVisible = false;
+  Icon passwordIcon = const Icon(Icons.visibility_off, color: Color(0XFF0D47A1),);
 
   void show() {
     setState(() {
@@ -36,39 +39,72 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          //Evita o erro de overflow
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 60),
-                Image.asset('assets/logo-softinsa.png'),
-                const SizedBox(height: 90),
-                TextField(
-                  decoration: InputDecoration(
-                    border: UnderlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 60),
+                  Image.asset('assets/logo-softinsa.png'),
+                  const SizedBox(height: 90),
+                  SizedBox(
+                    width: 374,
+                    height: 46,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: const Color(0XFFECEFF1),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'Email',
+                      ),
                     ),
-                    labelText: 'Email',
                   ),
-                ),
-                const SizedBox(height: 25),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: UnderlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+                  const SizedBox(height: 25),
+                  SizedBox(
+                    width: 374,
+                    height: 46,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          onPressed: (){}, 
+                          icon: icon //FICAMOS NO MINUTO 3:44 NO VIDEO "ADD ICON WITH TEXTFIELD IN FLUTTER" 
+                        )
+                        fillColor: const Color(0XFFECEFF1),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'Password',
+                      ),
                     ),
-                    labelText: 'Password',
                   ),
-                ),
-                const SizedBox(height: 100),
-                ElevatedButton(onPressed: show, child: const Text('Login')),
-                Text(text),
-              ],
+                  const SizedBox(height: 90),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0XFF0D47A1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      fixedSize: const Size(310, 46),
+                    ),
+                    onPressed: show,
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Text(text),
+                ],
+              ),
             ),
           ),
         ),
