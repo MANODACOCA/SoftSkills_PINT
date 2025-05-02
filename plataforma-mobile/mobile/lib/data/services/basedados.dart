d3d39445665890de54b7e94ba614c24791648773
Catarina Costa <catarinac.2005@gmail.com>
2025-05-02 18:21:06 +0100

    iniciei a base de dados mas parei visto que quando o power design colaborar com nossas personas vamos tentar extrair o script em sqflite

--- plataforma-mobile/mobile/lib/data/services/basedados.dart
+++ plataforma-mobile/mobile/lib/data/services/basedados.dart
@@ -2,5 +2,35 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class Basedados {
-  
+  static const bdsoftskills = "bdsoftskills.db";
+  final int versao = 1;
+  static Database? _basededados;
+  final bool estado = false;
+
+  Future<Database> get basededados async {
+    if(_basededados != null)
+      return _basededados!;
+    
+    _basededados = await _initDatabase();
+    return _basededados!;
+  } 
+  _initDatabase() async {
+    String path = join(await getDatabasesPath(), bdsoftskills);
+    return await openDatabase(path,
+      version: versao, onCreate: _onCreate);
+  }
+  Future _onCreate (Database db, int version) async {
+    
+  }
+  Future<void> createTables() async {
+    Database db = await basededados;
+
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS utilizadores (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome text,
+        pass TEXT
+      )
+    ''');
+  }
 }
