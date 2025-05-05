64f1dc14b20b1c8a61a1f25b326527e8f3139b5a
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-05-05 18:04:34 +0100

    Update
    Login page was created see if you like, so i have all the info you need on a profile home page, but i need to insert some elements but it's something easy to do

--- plataforma-mobile/mobile/lib/ui/profile/home.dart
+++ plataforma-mobile/mobile/lib/ui/profile/home.dart
@@ -3,6 +3,8 @@ import 'package:go_router/go_router.dart';
 import 'package:mobile/ui/core/themes/colors.dart';
 import '../core/shared/navigationbar_component.dart';
 
+//None of the buttons have a fuction, and i need to insert a right arrow
+
 class Profile extends StatefulWidget {
   const Profile({super.key});
 
@@ -27,17 +29,17 @@ class _ProfileState extends State<Profile> {
         centerTitle: true,
         backgroundColor: AppColors.primary,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(2),
-        child: GridView.count(
-          crossAxisCount: 2,
-          childAspectRatio: 0.75,
-          children: [
-            SizedBox(
-              width: 411,
-              height: 266,
-              child: Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(2),
+          child: Column(
+            children: [
+              SizedBox(height: 25),
+              SizedBox(
+                width: double.infinity,
+                height: 170,
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Container(
                       width: 100,
@@ -50,6 +52,7 @@ class _ProfileState extends State<Profile> {
                         ),
                       ),
                     ),
+                    SizedBox(height: 10),
                     Text(
                       "Nome do Utilizador",
                       style: TextStyle(
@@ -65,8 +68,210 @@ class _ProfileState extends State<Profile> {
                   ],
                 ),
               ),
-            ),
-          ],
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+                indent: 10,
+                endIndent: 10,
+              ),
+              SizedBox(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          SizedBox(width: 10),
+                          Text(
+                            'CONTA',
+                            style: TextStyle(
+                              color: Color.fromRGBO(121, 112, 133, 1),
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    SizedBox(
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(width: 18),
+                          Icon(
+                            Icons.person,
+                            color: const Color.fromARGB(255, 88, 85, 85),
+                          ),
+                          SizedBox(width: 5),
+                          Text('Alteração de dados pessoais'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(width: 18),
+                          Icon(
+                            Icons.key,
+                            color: const Color.fromARGB(255, 88, 85, 85),
+                          ),
+                          SizedBox(width: 5),
+                          Text('Informações de login'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+                indent: 10,
+                endIndent: 10,
+              ),
+              SizedBox(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          SizedBox(width: 10),
+                          Text(
+                            'CURSOS',
+                            style: TextStyle(
+                              color: Color.fromRGBO(121, 112, 133, 1),
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    SizedBox(
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(width: 18),
+                          Icon(
+                            Icons.computer,
+                            color: const Color.fromARGB(255, 88, 85, 85),
+                          ),
+                          SizedBox(width: 5),
+                          Text('Cursos inscritos'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(width: 18),
+                          Icon(
+                            Icons.flag_outlined,
+                            color: const Color.fromARGB(255, 88, 85, 85),
+                          ),
+                          SizedBox(width: 5),
+                          Text('Cursos terminados'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(width: 18),
+                          Icon(
+                            Icons.favorite,
+                            color: const Color.fromARGB(255, 88, 85, 85),
+                          ),
+                          SizedBox(width: 5),
+                          Text('Cursos que gostaste'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+                indent: 10,
+                endIndent: 10,
+              ),
+              SizedBox(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        SizedBox(width: 10),
+                        Text(
+                          'PREFERÊNCIAS',
+                          style: TextStyle(
+                            color: Color.fromRGBO(121, 112, 133, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    SizedBox(
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(width: 18),
+                          Icon(
+                            Icons.notifications,
+                            color: const Color.fromARGB(255, 88, 85, 85),
+                          ),
+                          SizedBox(width: 5),
+                          Text('Notificações'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+                indent: 10,
+                endIndent: 10,
+              ),
+              SizedBox(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        SizedBox(width: 18),
+                        Icon(
+                          Icons.contact_support,
+                          color: const Color.fromARGB(255, 88, 85, 85),
+                        ),
+                        SizedBox(width: 5),
+                        Text('Central de suporte'),
+                      ],
+                    ),
+                    SizedBox(height: 2),
+                    Row(
+                      children: <Widget>[
+                        SizedBox(width: 18),
+                        Icon(
+                          Icons.privacy_tip_outlined,
+                          color: const Color.fromARGB(255, 88, 85, 85),
+                        ),
+                        SizedBox(width: 5),
+                        Text('Politica de privacidade'),
+                      ],
+                    ),
+                    SizedBox(height: 2),
+                    Row(
+                      children: <Widget>[
+                        SizedBox(width: 18),
+                        Icon(Icons.logout, color: Colors.red),
+                        SizedBox(width: 5),
+                        Text('Encerrar sessão'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: Footer(),
