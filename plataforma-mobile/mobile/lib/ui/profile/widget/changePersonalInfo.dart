ce1a1744a82a7ceb51bcd0d7316ce38be5102f67
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-05-06 11:18:35 +0100

    Update
    Modificar a parte do gender

--- plataforma-mobile/mobile/lib/ui/profile/widget/changePersonalInfo.dart
+++ plataforma-mobile/mobile/lib/ui/profile/widget/changePersonalInfo.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gender_picker/source/enums.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile/ui/core/themes/colors.dart';
 import '../../core/shared/navigationbar_component.dart';
+import 'package:country_picker/country_picker.dart';
+import 'package:gender_picker/gender_picker.dart';
 
 class ChangePersonalInfo extends StatefulWidget {
   const ChangePersonalInfo({Key? key}) : super(key: key);
@@ -71,19 +74,23 @@ class _ChangePersonalInfoState extends State<ChangePersonalInfo> {
                       children: <Widget>[
                         SizedBox(height: 10),
                         SizedBox(
-                          width: 372,
-                          child: Text(
-                            'Nome',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.left,
+                          width: 450,
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                'Nome',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                            ],
                           ),
                         ),
-                        SizedBox(height: 10),
+                        SizedBox(height: 5),
                         SizedBox(
-                          width: 372,
+                          width: 400,
                           height: 38,
                           child: TextField(
                             decoration: InputDecoration(
@@ -93,6 +100,187 @@ class _ChangePersonalInfoState extends State<ChangePersonalInfo> {
                                 borderSide: BorderSide(color: Colors.black),
                               ),
                             ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  SizedBox(
+                    width: 450,
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Telemovél',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    width: 400,
+                    height: 38,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: '+351',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  SizedBox(
+                    width: 450,
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Data de Nascimento',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    width: 400,
+                    height: 38,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'dd/mm/aaaa',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  SizedBox(
+                    width: 450,
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'Morada',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    width: 400,
+                    height: 38,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Rua, nº, andar, código postal',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  SizedBox(
+                    width: 450,
+                    child: Column(
+                      children: <Widget>[
+                        Center(
+                          child: Row(
+                            children: <Widget>[
+                              SizedBox(
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      'País',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    SizedBox(height: 5),
+                                    SizedBox(
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          border: Border.all(
+                                            color: Colors.black,
+                                            width: 1,
+                                          ),
+                                          borderRadius: BorderRadius.circular(
+                                            4,
+                                          ),
+                                        ),
+                                        child: TextButton(
+                                          onPressed: () {
+                                            showCountryPicker(
+                                              context: context,
+                                              showPhoneCode: false,
+                                              onSelect: (Country country) {
+                                                print(
+                                                  'Selected country: ${country.flagEmoji} ${country.displayName}',
+                                                );
+                                              },
+                                            );
+                                          },
+                                          child: Text(
+                                            'Selecionar país',
+                                            style: TextStyle(
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              SizedBox(
+                                width: 225,
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      'Género',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    SizedBox(height: 5),
+                                    TextButton(
+                                      onPressed: () {},
+                                      child: GenderPickerWithImage(
+                                        onChanged: (Gender? gender) {},
+                                        selectedGender: Gender.Male, //Default
+                                        selectedGenderTextStyle: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        unSelectedGenderTextStyle: TextStyle(
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
