35f15274c8478b7aff26ec6f2091918585c4eaf8
Catarina Costa <catarinac.2005@gmail.com>
2025-05-06 13:58:00 +0100

    alguma coisa

--- plataforma-mobile/mobile/lib/ui/home/widgets/homepage.dart
+++ plataforma-mobile/mobile/lib/ui/home/widgets/homepage.dart
@@ -20,7 +20,7 @@ class _HomePage extends State<HomePage> {
         centerTitle: true,
         backgroundColor: AppColors.primary,
       ),
-      body: Column(
+      /* body: Column(
         children: [
           const SizedBox(height: 6),
           CarouselSlider(
@@ -73,19 +73,107 @@ class _HomePage extends State<HomePage> {
                     currentMembers: 12,
                     maxMembers: 80,
                   ),
-                  /* CardCourse(),
-            CardCourse(),
-            CardCourse(),
-            CardCourse(),
-            CardCourse(),
-            CardCourse(),
-            CardCourse(), */
                 ],
               ),
             ),
           ),
         ],
+      ), */
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 6),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 200.0,
+                viewportFraction: 0.90,
+                autoPlay: true,
+              ),
+              items: [
+                'assets/course-flutter.png',
+                'assets/course-flutter.png',
+                'assets/course-flutter.png',
+                'assets/course-flutter.png',
+                'assets/course-flutter.png',
+              ].map((path) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(path, fit: BoxFit.cover),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 0.75,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  CardCourse(
+                    title: 'Curso de C++',
+                    typeCourse: 'Assincrono',
+                    startDate: DateTime(2025, 5, 10),
+                    endDate: DateTime(2025, 6, 15),
+                    currentMembers: 12,
+                    maxMembers: 80,
+                  ),
+                  CardCourse(
+                    title: 'Curso de C++',
+                    typeCourse: 'Assincrono',
+                    startDate: DateTime(2025, 5, 10),
+                    endDate: DateTime(2025, 6, 15),
+                    currentMembers: 12,
+                    maxMembers: 80,
+                  ),
+                  CardCourse(
+                    title: 'Curso de C++',
+                    typeCourse: 'Assincrono',
+                    startDate: DateTime(2025, 5, 10),
+                    endDate: DateTime(2025, 6, 15),
+                    currentMembers: 12,
+                    maxMembers: 80,
+                  ),
+                  CardCourse(
+                    title: 'Curso de C++',
+                    typeCourse: 'Assincrono',
+                    startDate: DateTime(2025, 5, 10),
+                    endDate: DateTime(2025, 6, 15),
+                    currentMembers: 12,
+                    maxMembers: 80,
+                  ),
+                  CardCourse(
+                    title: 'Curso de C++',
+                    typeCourse: 'Assincrono',
+                    startDate: DateTime(2025, 5, 10),
+                    endDate: DateTime(2025, 6, 15),
+                    currentMembers: 12,
+                    maxMembers: 80,
+                  ),
+                  CardCourse(
+                    title: 'Curso de C++',
+                    typeCourse: 'Assincrono',
+                    startDate: DateTime(2025, 5, 10),
+                    endDate: DateTime(2025, 6, 15),
+                    currentMembers: 12,
+                    maxMembers: 80,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+
       bottomNavigationBar: Footer(),
     );
   }
