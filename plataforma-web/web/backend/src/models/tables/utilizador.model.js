94bd3b9c301d531cdb8c30cc0669711a0a3021ab
Rafael <rafael.rfs.silva95@gmail.com>
2025-05-06 23:47:54 +0100

    pouco ou nada feito

--- plataforma-web/web/backend/src/models/tables/utilizador.model.js
+++ plataforma-web/web/backend/src/models/tables/utilizador.model.js
@@ -1,1 +1,46 @@
 const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Utilizador = db.define('UTILIZADOR', {
+    ID_UTILIZADOR: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        
+    },
+    NOME_UTILIZADOR: {
+        type: Sequelize.STRING
+    },
+    PASSWORD: {
+        type: Sequelize.STRING
+    },
+    DATA_CRIACAO_UTILIZ: {
+        type: Sequelize.DATE
+    },
+    TELEMOVEL: {
+        type: Sequelize.INTEGER
+    },
+    GENERO: {
+        type: Sequelize.INTEGER
+    },
+    MORADA: {
+        type: Sequelize.STRING
+    },
+    PAIS: {
+        type: Sequelize.STRING
+    },
+    DATA_NASC: {
+        type: Sequelize.DATE
+    },
+    EMAIL: {
+        type: Sequelize.STRING
+    },
+    DATA_ATIV_UTILI: {
+        type: Sequelize.DATE
+    },
+    AUTEN2FAT: {
+        type: Sequelize.BOOLEAN
+    }
+});
+
+module.exports = Utilizador;
