36288f6b885a00033c726c2d25155d8c39ffd169
RSantiago2523 <rodrigorcsantiago@gmail.com>
2025-05-11 17:27:58 +0100

    Update app.js

--- plataforma-web/web/backend/src/app.js
+++ plataforma-web/web/backend/src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Main Route
 app.get('/', (req, res) => {
