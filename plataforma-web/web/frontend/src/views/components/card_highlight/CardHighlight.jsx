9e72d50edba4b58658b5a67ff7638140885d8db3
Rafael <rafael.rfs.silva95@gmail.com>
2025-05-14 14:18:07 +0100

    update card normal e destaque e alt padding home page

--- plataforma-web/web/frontend/src/views/components/card_highlight/CardHighlight.jsx
+++ plataforma-web/web/frontend/src/views/components/card_highlight/CardHighlight.jsx
@@ -2,40 +2,34 @@
 import './CardHighlight.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaCalendarAlt, FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 import Img1 from '../../../assets/images/logos/comfundo.png';
 
-const FeaturedCourseCard = ({
-  image,
-  title,
-  description,
-  startDate,
-  endDate,
-  instructor,
-  instructorImage,
-}) => {
+const FeaturedCourseCard = ({ image, title, description, startDate, endDate, instructor, instructorImage, courseId }) => {
+  const navigate = useNavigate();
+
+  const goToCourse = () => {
+    navigate(`/curso/${courseId}`);
+  };
 
   return (
-    <div className="card flex-row p-3 rounded-4" style={{ backgroundColor: '#e8f0fb' }}>
-      <img src={Img1} className="rounded-4" alt="imagem curso" style={{ width: '280px', height: 'auto', objectFit: 'cover' }} />
+    <div className="card flex-row rounded-4 card-highlight">
+      <img src={Img1} className="rounded-4 highlight-image" alt="imagem curso" />
       <div className="card-body d-flex flex-column justify-content-between">
         <div>
-          <h5 className="card-title fw-bold">{'Geração de Videos com Inteligência Artificial'}</h5>
-          <p className="card-text text-muted" style={{ whiteSpace: 'pre-line' }}>
-                    {'Explore o pontecial da IA generativa para criar videos incriveis!\nGere videos em diversos estilos e formatos'}</p>
+          <h4 className="card-title">{'Geração de Videos com Inteligência Artificial'}</h4>
+          <p className="card-text text-muted fs-4 truncate-text">
+            {'Explore o pontecial da IA generativa para criar videos incriveis! Gere videos em diversos estilos e formatos Explore o pontecial da IA generativa para criar videos incriveis! Gere videos em diversos estilos e formatos Explore o pontecial da IA generativa para criar videos incriveis! Gere videos em diversos estilos e formatos'}</p>
+          <p className="card-text text-muted mb-0 fs-6">
+            📅 {'10 Dez'} - {'15 Jan'}
+          </p>
           <div className="d-flex align-items-center mb-2">
-            <FaCalendarAlt className="me-2 text-secondary" />
-            <span>{'22 Dez'} - {'14 Jan'}</span>
-          </div>
-          <div className="d-flex align-items-center mb-2">
-            <img src={Img1} alt="instrutor" className="rounded-circle me-2" style={{ width: '32px', height: '32px' }} />
+            <img src={Img1} alt="instrutor" className="rounded-circle me-2 avatar" />
             <span>{'Kevin Gilbert'}</span>
           </div>
         </div>
-        <div className="d-flex justify-content-between align-items-center mt-2">
-          <div className="d-flex align-items-center">
-          </div>
-          <button className="btn btn-primary px-4 rounded-4">Detalhes</button>
+        <div className="mt-2">
+          <button className="btn btn-primary px-4 rounded-4" onClick={goToCourse} >Detalhes</button>
         </div>
       </div>
     </div>
--- plataforma-web/web/frontend/src/views/pages/home/homepage.jsx
+++ plataforma-web/web/frontend/src/views/pages/home/homepage.jsx
@@ -21,10 +21,11 @@ const HomePage = () => {
 
   return (
     <div>
-      <Slide />
-      <h3 className='mt-2'>Bem vindo à home da nossa amostra de página da softskills</h3>
+      <div className='px-3'> <Slide /></div>
 
-      <h1 className="mt-4 ps-3">Para si</h1>
+      <h1 className='mt-3 px-3'>Bem vindo à home da nossa amostra de página da softskills</h1>
+
+      <h1 className="mt-3 ps-3">Para si</h1>
       <div className="position-relative px-3">
         <button className="btn btn-light position-absolute top-50 start-0 translate-middle-y z-1 arrow-click"
           onClick={() => scroll(scrollRef, 'left')}>
@@ -45,7 +46,7 @@ const HomePage = () => {
       </div>
 
 
-      <h1 className="mt-4 ps-3">Cursos mais populares</h1>
+      <h1 className="mt-3 ps-3">Cursos mais populares</h1>
       <div className="position-relative px-3">
         <button className="btn btn-light position-absolute top-50 start-0 translate-middle-y z-1 arrow-click"
           onClick={() => scroll(scrollRef1, 'left')}>
@@ -65,10 +66,10 @@ const HomePage = () => {
         </button>
       </div>
 
-      <h1 className="mt-5 ps-3">Curso assíncrono em destaque</h1>
-      <Cardhighlight />
+      <h1 className="mt-3 ps-3">Curso assíncrono em destaque</h1>
+      <div className='px-3'><Cardhighlight /></div>
 
-      <h1 className="mt-4 ps-3">Novidades</h1>
+      <h1 className="mt-3 ps-3">Novidades</h1>
       <div className="position-relative px-3">
         <button className="btn btn-light position-absolute top-50 start-0 translate-middle-y z-1 arrow-click"
           onClick={() => scroll(scrollRef2, 'left')}>
@@ -88,8 +89,8 @@ const HomePage = () => {
         </button>
       </div>
 
-      <h1 className="mt-5 ps-3">Curso síncrono em destaque</h1>
-      <Cardhighlight />
+      <h1 className="mt-3 ps-3">Curso síncrono em destaque</h1>
+      <div className='px-3'><Cardhighlight /></div>
     </div>
   );
 };
