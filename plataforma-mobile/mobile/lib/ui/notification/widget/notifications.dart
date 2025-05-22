73a335c2b9df1471caf39d3130bf2413de768c71
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-05-22 18:01:16 +0100

    update

--- plataforma-mobile/mobile/lib/ui/notification/widget/notifications.dart
+++ plataforma-mobile/mobile/lib/ui/notification/widget/notifications.dart
@@ -1,6 +1,8 @@
+import 'package:go_router/go_router.dart';
+import '../../core/shared/search_bar.dart';
+import '../../core/shared/export.dart';
 import '../../core/shared/export.dart';
 import '../../core/shared/navigationbar_component.dart';
-
 
 class Notification extends StatefulWidget {
   const Notification({super.key});
@@ -13,7 +15,14 @@ class _NotificationState extends State<Notification> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Notificaçoes'),
+      appBar: AppBar(
+        title: SearchBarCustom(),
+        backgroundColor: AppColors.primary,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(children: <Widget>[Text('Lista das Notificações')]),
+      ),
       bottomNavigationBar: Footer(),
     );
   }
