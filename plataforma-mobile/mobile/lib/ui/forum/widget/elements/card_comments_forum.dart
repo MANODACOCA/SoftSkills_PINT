56b786e5e6c6dc0aa87a5b79c61f0adadd02ba4c
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-06-22 13:38:58 +0100

    update
    Introduzi a funcao de poder copiar o texto ou denunciar, mas na parte de denuncia ainda nao fiz a logica, mas ja copia o texto

--- plataforma-mobile/mobile/lib/ui/forum/widget/elements/card_comments_forum.dart
+++ plataforma-mobile/mobile/lib/ui/forum/widget/elements/card_comments_forum.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: avoid_print
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:like_button/like_button.dart';
 import 'package:mobile/ui/core/shared/export.dart';
@@ -21,6 +22,8 @@ class Post extends StatelessWidget {
   final String description;
   final String photo;
   final bool selectComment;
+  final TextEditingController _copiar = TextEditingController();
+  final TextEditingController _denunciar = TextEditingController();
 
   late int likes = forumLike;
 
@@ -61,6 +64,43 @@ class Post extends StatelessWidget {
                 '${DateTime.now().toLocal().toString().substring(0, 10)}', //Change to database date
                 style: TextStyle(fontSize: 16, color: Colors.grey),
               ),
+              trailing: Transform.translate(
+                offset: Offset(-16, 0), // Ajusta horizontalmente
+                child: PopupMenuButton<String>(
+                  offset: Offset(-20, 0.5), // Ajusta verticalmente
+                  position: PopupMenuPosition.under,
+                  icon: Icon(Icons.more_vert, color: Colors.grey),
+                  onSelected: (value) {
+                    if (value == 'copiar') {
+                      _copiar.text = description;
+                      Clipboard.setData(ClipboardData(text: _copiar.text));
+                    } else if (value == 'denunciar') {}
+                  },
+                  itemBuilder:
+                      (BuildContext context) => [
+                        PopupMenuItem(
+                          value: 'copiar',
+                          child: Row(
+                            children: [
+                              Icon(Icons.copy, color: Colors.grey),
+                              SizedBox(width: 8),
+                              Text('Copy'),
+                            ],
+                          ),
+                        ),
+                        PopupMenuItem(
+                          value: 'denunciar',
+                          child: Row(
+                            children: [
+                              Icon(Icons.flag, color: Colors.grey),
+                              SizedBox(width: 8),
+                              Text('Report'),
+                            ],
+                          ),
+                        ),
+                      ],
+                ),
+              ),
             ),
           ),
           Padding(
@@ -73,7 +113,7 @@ class Post extends StatelessWidget {
           SizedBox(
             child: Row(
               children: [
-                SizedBox(width: 15,),
+                SizedBox(width: 15),
                 LikeButton(
                   size: 30,
                   likeCount: likes,
@@ -97,23 +137,26 @@ class Post extends StatelessWidget {
                   children: [
                     IconButton(
                       isSelected: selectComment,
-                      icon: Icon(Icons.comment, color: selectComment ? AppColors.secondary : Colors.grey,),
+                      icon: Icon(
+                        Icons.comment,
+                        color:
+                            selectComment ? AppColors.secondary : Colors.grey,
+                      ),
                       onPressed: () {
-                        context.push('/commentPage', extra: {
-                          'postName': forumName,
-                          'description': description,
-                          'likes': likes,
-                          'comments': forumComments,
-                          'photo': photo,
-                        });
+                        context.push(
+                          '/commentPage',
+                          extra: {
+                            'postName': forumName,
+                            'description': description,
+                            'likes': likes,
+                            'comments': forumComments,
+                            'photo': photo,
+                          },
+                        );
                       },
                     ),
                     SizedBox(height: 5),
                   ],
-                ),
-                Text(
-                  '$forumComments',
-                  style: TextStyle(color: Colors.grey, fontSize: 16),
                 ),
               ],
             ),
