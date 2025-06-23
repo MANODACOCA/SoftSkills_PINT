3f731d3fe22cfc1b420af4478c06d0be428a3e3b
Catarina Costa <catarinac.2005@gmail.com>
2025-06-23 02:02:12 +0100

    algo

---
+++ plataforma-web/web/frontend/src/views/components/table/ColumnsAula.jsx
@@ -0,0 +1,16 @@
+import { renderMatches } from "react-router-dom";
+import { formatDayMonthYear } from "../shared_functions/FunctionsUtils";
+
+
+export const columnsAulas = [
+    { label: 'Nº', key: 'id_aula'},
+    { label: 'Nome Aula', render: (item) => { return item.nome_aula; }},
+    { label: 'Video', render: (item) => {
+        if(item.caminho_url != null) {
+            return 'Já contém aula';
+        }
+        else return 'Não contém aula';
+    }},
+    { label: 'Tempo Duracao', render: (item) => { return 'funcao em falta e falta buscar tabela';}},
+];
+
