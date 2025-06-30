b490bb52fd456fa3a352a81d2ab599af8005e02a
MANODACOCA <dfso2013@gmail.com>
2025-06-30 14:04:03 +0100

    teste

--- plataforma-web/web/backend/src/services/conteudo_partilhado.service.js
+++ plataforma-web/web/backend/src/services/conteudo_partilhado.service.js
@@ -1,31 +1,47 @@
 const { Sequelize, Op, where } = require('sequelize');
 const sequelize = require('../models/database');
-const { conteudos_partilhado, topico} = require('../models/init-models')(sequelize);
+const { conteudos_partilhado, topico } = require('../models/init-models')(sequelize);
 
-async function getForuns(ordenar = "Mais Recentes") {
-    let foruns = [];
-    let order = [];
+async function getForuns(ordenar = "Mais Recentes", search = "") {
+  let foruns = [];
+  let order = [];
+  let whereTopico = {};
 
-    if (ordenar === "Mais Antigos") {
-        order = [['data_criacao_cp', 'ASC']];
-    } else if (ordenar === "Mais Recentes") {
-        order = [['data_criacao_cp', 'DESC']];
-    }
+  if (ordenar === "Mais Antigos") {
+    order = [['data_criacao_cp', 'ASC']];
+  } else if (ordenar === "Mais Recentes") {
+    order = [['data_criacao_cp', 'DESC']];
+  }
 
-    foruns = await conteudos_partilhado.findAll({
-        include: [
-            {
-                model: topico,
-                as: "id_topico_topico",
-                attributes: ['nome_topico', 'descricao_top'],
-            }
-        ],
-        order
-    });
+  if (search) {
 
-    return foruns;
+    const unaccentedSearch = search.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    const searchFilter = Sequelize.where(
+      Sequelize.fn('unaccent', Sequelize.col('id_topico_topico.nome_topico')),
+      {
+        [Op.iLike]: `%${unaccentedSearch}%`
+      }
+    );
+
+   whereTopico = searchFilter;
+  }
+
+  foruns = await conteudos_partilhado.findAll({
+    include: [
+      {
+        model: topico,
+        as: "id_topico_topico",
+        attributes: ['nome_topico', 'descricao_top'],
+        where: Object.keys(whereTopico).length ? whereTopico : undefined
+      }
+    ],
+    order
+  });
+
+  return foruns;
 }
 
 module.exports = {
-    getForuns
+  getForuns
 };
