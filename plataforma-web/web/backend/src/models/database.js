ec9774ff0fb6a3f7f0e1a047cda7274fec4b8b5f
MANODACOCA <dfso2013@gmail.com>
2025-07-07 00:34:12 +0100

    awdawdawd

--- plataforma-web/web/backend/src/models/database.js
+++ plataforma-web/web/backend/src/models/database.js
@@ -9,17 +9,11 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       require: true,
       rejectUnauthorized: false,
     }
-  },
-  hooks: {
-    afterConnect: async (connection) => {
-      // Define o timezone para cada nova conexão
-      await connection.query("SET TIME ZONE 'Europe/Lisbon';");
-    }
   }
 });
 
 sequelize.authenticate()
-  .then(() => console.log('Conexão com base de dados realizada com sucesso!'))
-  .catch(error => console.log('Erro ao conectar com base de dados:', error));
+    .then(()=> console.log('Conexão com base de dados realizada com sucesso!'))
+    .catch(error=> console.log('Erro ao conectar com base de dados:', error));
 
 module.exports = sequelize;
