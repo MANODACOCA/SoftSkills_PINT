e20c2ce109972323826a74d16bb752c3c5db338a
MANODACOCA <dfso2013@gmail.com>
2025-07-07 16:21:28 +0100

    adawadwa

--- plataforma-web/web/backend/src/services/resultados.service.js
+++ plataforma-web/web/backend/src/services/resultados.service.js
@@ -1,40 +1,44 @@
 const sequelize = require('../models/database');
-const { utilizador, formandos, resultados } = require('../models/init-models')(sequelize);
+const { utilizador, formandos, resultados, inscricoes } = require('../models/init-models')(sequelize);
 
-async function getResultadosFormandos(cursoId){
-    try{
-        const resultadosFinais = await resultados.findAll({
-            where: {id_curso_sincrono : cursoId},
-                include: [
-                    {
-                        model: formandos,
-                        as: 'id_formando_formando',
-                        attributes: ['id_formando'],
-                        include: [
-                            {
-                                model: utilizador,
-                                as: 'id_formando_utilizador',
-                                attributes: [
-                                    [sequelize.col('id_utilizador'), 'id_util'],
-                                    [sequelize.col('nome_utilizador'), 'nome_util']
-                                ],
-                            }
-                        ]   
-                    }
-                ],
-                order: [[
-                    { model: formandos, as: 'id_formando_formando'},
-                    { model: utilizador, as: 'id_formando_utilizador'},
-                    'nome_utilizador']]    
-            });
+async function getResultadosFormandosInscritos(cursoId) {
+    try {
+        const InscritosComResultadosFinais = await inscricoes.findAll({
+            where: { id_curso: cursoId },
+            include: [
+                {
+                    model: formandos,
+                    as: 'id_formando_formando',
+                    attributes: ['id_formando'],
+                    include: [
+                        {
+                            model: resultados,
+                            as: 'resultados',
 
-        return resultadosFinais;
-    } catch(error) {
+                            model: utilizador,
+                            as: 'id_formando_utilizador',
+                            attributes: [
+                                [sequelize.col('id_utilizador'), 'id_util'],
+                                [sequelize.col('nome_utilizador'), 'nome_util']
+                            ],
+
+                        }
+                    ]
+                }
+            ],
+            order: [[
+                { model: formandos, as: 'id_formando_formando' },
+                { model: utilizador, as: 'id_formando_utilizador' },
+                'nome_utilizador']]
+        });
+
+        return InscritosComResultadosFinais;
+    } catch (error) {
         console.error('Erro ao buscar resultados', error);
         throw error;
     }
 }
 
 module.exports = {
-    getResultadosFormandos
+    getResultadosFormandosInscritos
 }; 
