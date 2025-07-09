cca6e55364550afeb76914fee2a8b383ae8ba9ef
Catarina Costa <catarinac.2005@gmail.com>
2025-07-09 19:09:52 +0100

    o que importa e ser feliz

--- plataforma-mobile/mobile/lib/ui/trabalhos/entrega_trabalho.dart
+++ plataforma-mobile/mobile/lib/ui/trabalhos/entrega_trabalho.dart
@@ -3,6 +3,7 @@ import 'package:mobile/ui/core/shared/base_comp/app_bar_arrow.dart';
 import 'package:mobile/ui/core/shared/base_comp/text_expand.dart';
 import 'package:mobile/ui/core/shared/trabalhos/box_submit_trab.dart';
 import 'package:mobile/ui/core/shared/trabalhos/card_enunciado.dart';
+import 'package:mobile/utils/uteis.dart';
 import '../core/shared/export.dart';
 
 class EntregaTrabalho extends StatefulWidget {
@@ -17,45 +18,69 @@ class EntregaTrabalho extends StatefulWidget {
 class _EntregaTrabalhoState extends State<EntregaTrabalho> {
     @override
     Widget build(BuildContext context) {
+      final agora = DateTime.now();
+      final dataEntregaStr = widget.trabalho['data_entrega_tr'];
+      final dataEntrega = DateTime.tryParse(dataEntregaStr);
+
         return AppScaffold(
-            appBar: AppBarArrow(title: widget.trabalho['nome_tr'], onBack: () => context.pop()),
-            body: Column(
-                children: [
-                    SizedBox(height: 10,),
-                    Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                        'Descrição',
-                        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                        ),
-                    ),
-                    SizedBox(height: 10,),
-                    Container(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        width: double.infinity,
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                                TextExpand(text: widget.trabalho['descricao_tr'],),  
-                            ],
-                        ), 
-                    ),
-                    SizedBox(height: 20,),
-                    CardEnunciado(enunciado: widget.trabalho),
-                    SizedBox(height: 20,),
-                    Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                        'Entrega',
-                        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                        ),
-                    ),
-                    SizedBox(height: 15,),
-                    BoxSubmitTrab(idCurso: widget.trabalho['id_curso_tr'],idTrabalho: widget.trabalho['id_trabalho'],),
-                ],
-            ),
-        );
+          appBar: AppBarArrow(title: widget.trabalho['nome_tr'], onBack: () => context.pop()),
+          body: Column(
+            children: [
+              SizedBox(height: 10,),
+              Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                  'Descrição:',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                          TextExpand(text: widget.trabalho['descricao_tr'],),  
+                      ],
+                  ), 
+              ),
+              SizedBox(height: 20,),
+              Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                  'Rquisitos:',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+              ),
+              SizedBox(height: 10,),
+              CardEnunciado(enunciado: widget.trabalho),
+              SizedBox(height: 20,),
+              Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                  'Data e hora de entrega:',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text('${formatDateRangeSingle(widget.trabalho['data_entrega_tr'])} | '),
+              ),
+              SizedBox(height: 20,),
+              if (dataEntrega != null && dataEntrega.isAfter(agora)) ...[
+                BoxSubmitTrab(idCurso: widget.trabalho['id_curso_tr'],idTrabalho: widget.trabalho['id_trabalho'], expirado: false,),                  
+              ] else ...[
+                BoxSubmitTrab(idCurso: widget.trabalho['id_curso_tr'],idTrabalho: widget.trabalho['id_trabalho'], expirado: true,),
+                Text('O prazo de entrega já expirou', style: TextStyle(color: Colors.red),),
+              ],
+            ],
+          ),
+      );
     }
 }
