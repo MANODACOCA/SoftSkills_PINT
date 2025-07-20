571855485d90f6a563771d7925b998e13d261d6b
Catarina Costa <catarinac.2005@gmail.com>
2025-07-20 22:28:41 +0100

    merdas

--- plataforma-web/web/frontend/src/views/pages/admin/forum/forumTable.jsx
+++ plataforma-web/web/frontend/src/views/pages/admin/forum/forumTable.jsx
@@ -6,8 +6,13 @@ import { useNavigate } from "react-router-dom";
 
 const ForumTable = () => {
     const [forum, setforum] = useState([]);
+    const opcoes = ['Fóruns', 'Pedidos'];
+    const [opcao, setOpcao] = useState("Fóruns");
     const navigate = useNavigate();
 
+    const handleChangeOpcao = (a) => {
+        setOpcao(a);
+    };
     const FetchForum = async () => {
         try {
             const response = await list_conteudos_partilhado();
@@ -37,7 +42,34 @@ const ForumTable = () => {
 
     return(
         <div>
-            <Table columns={columnsForum} data={forum} />
+            <div className="mb-3 d-flex justify-content-between">
+                <div>
+                    {opcao === 'Fóruns' && (
+                    <h3>Lista tópicos fórum</h3>
+                    )}
+                    {opcao === 'Pedidos' && (
+                    <h3>Pedidos de tópicos fórum</h3>
+                    )}
+                </div>
+                <div className="btn-group w-25">
+                    {opcoes.map((o, index) => (
+                    <button
+                        key={index}
+                        className={`btn ${opcao === o ? 'btn-active' : 'btn-outline-custom'}`}
+                        onClick={() => handleChangeOpcao(o)}
+                    >
+                        {o.charAt(0).toUpperCase() + o.slice(1)}
+                    </button>
+                    ))}
+                </div>    
+            </div>
+            
+            {opcao === 'Fóruns' && (
+               <Table columns={columnsForum} data={forum} /> 
+            )}
+            {opcao === 'Pedidos' && (
+               <div>olaaaa</div>
+            )}
         </div>
     );
 }
