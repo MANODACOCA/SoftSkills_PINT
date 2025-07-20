7ea0276672fc324c6b6b9c259b2f5a48620e1707
MANODACOCA <dfso2013@gmail.com>
2025-07-20 16:58:25 +0100

    awdawdaw

--- plataforma-web/web/frontend/src/views/components/base_components/footer/Footer_Inicial.jsx
+++ plataforma-web/web/frontend/src/views/components/base_components/footer/Footer_Inicial.jsx
@@ -34,8 +34,8 @@ const FooterIncial = () => {
                                 <h6 className="text-uppercase fw-bold">CONTACTOS</h6>
                                 <div className="d-flex flex-column">
                                     <h6><strong>Email:</strong> suporte@softinsa.com</h6>
-                                    <h6><strong>Telefone:</strong> 213219600</h6>
-                                    <h6><strong>Morada:</strong> Zona Industrial de Coimbrões 16b <br />
+                                    <h6><strong>Telefone:</strong> 213555444</h6>
+                                    <h6><strong>Morada:</strong> Zona Industrial de Coimbrões 160G <br />
                                         3500-618 Viseu</h6>
                                 </div>
                             </div>
