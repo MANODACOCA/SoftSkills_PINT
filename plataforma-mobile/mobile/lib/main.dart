b95f40a5f6325bd92016cc3f04a072fb3e44e1d4
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-07-21 13:11:34 +0100

    nada de novo
    amanha 8:30 com o tomezinho

--- plataforma-mobile/mobile/lib/main.dart
+++ plataforma-mobile/mobile/lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:permission_handler/permission_handler.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mobile/Firebase/firebase_api.dart';
-import 'package:mobile/Firebase/flutter_notificartions.dart';
 import 'package:mobile/provider/auth_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -11,27 +7,10 @@ import 'package:go_router/go_router.dart';
 import 'services/auth_service.dart';
 import 'ui/core/shared/export.dart';
 
-Future<void> requestNotificationPermission() async {
-  if (await Permission.notification.request().isGranted) {
-    print('========================================');
-    print('Permissão para notificações concedida');
-    print('========================================');
-  } else {
-    print('========================================');
-    print('Permissão para notificações NEGADA');
-    print('========================================');
-
-  }
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
-  await setupFlutterNotifications();
   await FirebaseAPI().initNotifications();
-
-  await requestNotificationPermission();
 
   debugPrintGlobalKeyedWidgetLifecycle = true;
   await initializeDateFormatting('pt_PT', null);
