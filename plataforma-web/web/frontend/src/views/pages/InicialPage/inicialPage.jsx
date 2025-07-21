0b3e07cf227a16890c919680441501a2cd8573a3
MANODACOCA <dfso2013@gmail.com>
2025-07-21 02:23:02 +0100

    jjjj

--- plataforma-web/web/frontend/src/views/pages/InicialPage/inicialPage.jsx
+++ plataforma-web/web/frontend/src/views/pages/InicialPage/inicialPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HeaderInicial from '../../components/base_components/header/Header_Inicial';
 import PageInicial from './PageInicial';
 import FooterInicial from '../../components/base_components/footer/Footer_Inicial';
 
+
+
 const InicialPage = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <>
             <HeaderInicial />
