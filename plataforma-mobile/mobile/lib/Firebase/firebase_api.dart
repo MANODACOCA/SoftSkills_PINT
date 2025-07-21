b23b6800392e842ba419a636f02e9732006b495d
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-07-21 11:50:50 +0100

    update
    Usei o Chat para me ajudar para receber notificacoes, o projeto abre, mas as notificacoes funciona

--- plataforma-mobile/mobile/lib/Firebase/firebase_api.dart
+++ plataforma-mobile/mobile/lib/Firebase/firebase_api.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:mobile/Firebase/flutter_notificartions.dart';
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('============================');
   print('Title: ${message.notification?.title}');
   print('Body: ${message.notification?.body}');
   print('Payload: ${message.data}');
+  print('============================');
 }
 
 class FirebaseAPI {
@@ -13,10 +16,24 @@ class FirebaseAPI {
 
   Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
-    final TCMToken = await _firebaseMessaging.getToken();
+
+    final fcmToken = await FirebaseMessaging.instance.getToken();
     print('============================');
-    print('Token: $TCMToken');
+    print('Token: $fcmToken');
     print('============================');
-    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
+    print('============================');
+    print('Registrando listener onMessage...');
+    print('============================');
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('============================');
+      print('Mensagem RECEBIDA em foreground');
+      print('Title: ${message.notification?.title}');
+      print('Body: ${message.notification?.body}');
+      print('Data: ${message.data}');
+      print('============================');
+      showLocalNotification(message);
+    });
   }
 }
--- plataforma-mobile/mobile/lib/main.dart
+++ plataforma-mobile/mobile/lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:mobile/Firebase/firebase_api.dart';
+import 'package:mobile/Firebase/flutter_notificartions.dart';
 import 'package:mobile/provider/auth_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -8,13 +11,32 @@ import 'package:go_router/go_router.dart';
 import 'services/auth_service.dart';
 import 'ui/core/shared/export.dart';
 
+Future<void> requestNotificationPermission() async {
+  if (await Permission.notification.request().isGranted) {
+    print('========================================');
+    print('Permissão para notificações concedida');
+    print('========================================');
+  } else {
+    print('========================================');
+    print('Permissão para notificações NEGADA');
+    print('========================================');
+
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+  await setupFlutterNotifications();
   await FirebaseAPI().initNotifications();
+
+  await requestNotificationPermission();
+
   debugPrintGlobalKeyedWidgetLifecycle = true;
   await initializeDateFormatting('pt_PT', null);
   await authService.init();
+
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
@@ -31,7 +53,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // construído uma única vez
   late final GoRouter _router = rotas;
 
   @override
