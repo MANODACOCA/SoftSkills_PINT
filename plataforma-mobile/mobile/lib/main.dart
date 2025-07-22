8a945fa74e488036025261a9376606fa30212105
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-07-22 12:23:47 +0100

    update
    ainda nao funciona, mas tenho tido a ajuda do professor

--- plataforma-mobile/mobile/lib/main.dart
+++ plataforma-mobile/mobile/lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mobile/Firebase/firebase_api.dart';
 import 'package:mobile/provider/auth_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:go_router/go_router.dart';
 import 'services/auth_service.dart';
 import 'ui/core/shared/export.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'firebase_options.dart'; // Gerado com `flutterfire configure`
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await FirebaseAPI().initNotifications();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await _initializeLocalNotifications();
 
   debugPrintGlobalKeyedWidgetLifecycle = true;
   await initializeDateFormatting('pt_PT', null);
@@ -24,6 +35,37 @@ void main() async {
   );
 }
 
+Future<void> _initializeLocalNotifications() async {
+  const AndroidInitializationSettings androidSettings =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const DarwinInitializationSettings iosSettings =
+      DarwinInitializationSettings();
+
+  await flutterLocalNotificationsPlugin.initialize(
+    const InitializationSettings(android: androidSettings, iOS: iosSettings),
+  );
+}
+
+void showLocalNotification(RemoteMessage message) async {
+  const AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+    'default_channel',
+    'Default',
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+
+  const NotificationDetails platformDetails = NotificationDetails(
+    android: androidDetails,
+  );
+
+  await flutterLocalNotificationsPlugin.show(
+    message.hashCode,
+    message.notification?.title ?? 'Notificação',
+    message.notification?.body ?? '',
+    platformDetails,
+  );
+}
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -33,6 +75,47 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late final GoRouter _router = rotas;
+
+  String mensagem = 'Aguardando notificações...';
+  @override
+  void initState() {
+    super.initState();
+    _setupFCM();
+  }
+
+  void _setupFCM() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    /*NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+*/
+    //print('Permission: ${settings.authorizationStatus}');
+
+    // Foreground
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      //print('Foreground message: ${message.notification?.title}');
+      if (message.notification != null) {
+        showLocalNotification(message);
+        setState(() {
+          mensagem = message.notification!.body!;
+        });
+      }
+    });
+
+    // App aberta por notificação
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      //print('Notification opened: ${message.notification?.title}');
+    });
+
+    //Token
+    String? token = await messaging.getToken();
+    print('============================');
+    print('FCM Token: $token');
+    print('============================');
+  }
 
   @override
   Widget build(BuildContext context) {
