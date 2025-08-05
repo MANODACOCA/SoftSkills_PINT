3e9beb083a8f8a793d9fc7c60f0f8ef8f48bd1c7
MANODACOCA <dfso2013@gmail.com>
2025-08-05 03:29:44 +0100

    dd

--- plataforma-web/web/frontend/src/api/avaliacoes_et_axios.js
+++ plataforma-web/web/frontend/src/api/avaliacoes_et_axios.js
@@ -1,54 +1,54 @@
 import axios from 'axios';
 
-const API_URL = 'https://softskills-api.onrender.com/certificados';
+const API_URL = 'https://softskills-api.onrender.com/avaliacoes_et';
 
 
-export const list_certificados = async () => {
+export const list_avaliacoes_et = async () => {
   try{
     const response = await axios.get(`${API_URL}/list`);
     return response.data;
   }catch (error){
-    console.error('Erro ao ir buscar lista de Certificados!');
+    console.error('Erro ao ir buscar lista de avaliacao de trabalhos!');
     throw error;
   }
 }
 
-export const get_certificados  = async (id) => {
+export const get_avaliacoes_et = async (id_entrega_trabalho) => {
     try{
-        const response = await axios.get(`${API_URL}/get/${id}`);
+        const response = await axios.get(`${API_URL}/get/${id_entrega_trabalho}`);
         return response.data;
     }catch(error){
-        console.error('Erro ao buscar Certificados!');
+        console.error('Erro ao buscar avaliacao de trabalho!');
         throw error;
     }
 };
 
-export const create_certificados = async (data) => {
+export const create_avaliacoes_et = async (data) => {
     try{
         const response = await axios.post(`${API_URL}/create`, data);
         return response.data;
     }catch(error){
-        console.error('Erro ao criar Certificados!');
+        console.error('Erro ao criar avaliacao de trabalho!');
         throw error;
     }
 };
 
-export const update_certificados = async (id, data) => {
+export const update_avaliacoes_et = async (id_entrega_trabalho, data) => {
     try{
-        const response = await axios.put(`${API_URL}/update/${id}`, data);
+        const response = await axios.put(`${API_URL}/update/${id_entrega_trabalho}`, data);
         return response.data;
     }catch(error){
-        console.error('Erro ao atualizar Certificados!');
+        console.error('Erro ao atualizar avaliacao de trabalho!');
         throw error;
     }
 };
 
-export const delete_certificados = async (id) => {
+export const delete_avaliacoes_et = async (id_entrega_trabalho) => {
     try{
-        const response = await axios.delete(`${API_URL}/delete/${id}`);
+        const response = await axios.delete(`${API_URL}/delete/${id_entrega_trabalho}`);
         return response.data;
     }catch(error){
-        console.error('Erro ao excluir Certificados!');
+        console.error('Erro ao excluir avaliacao de trabalho!');
         throw error;
     }
 };
