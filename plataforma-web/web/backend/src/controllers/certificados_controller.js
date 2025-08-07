336809a1a8bd0284e7b9fd8cea0b5cd8580c0ea1
Rafael <rafael.rfs.silva95@gmail.com>
2025-08-07 16:53:32 +0100

    q

--- plataforma-web/web/backend/src/controllers/certificados_controller.js
+++ plataforma-web/web/backend/src/controllers/certificados_controller.js
@@ -45,7 +45,10 @@ controllers.gerarCertificado = async (req, res) => {
       notaFinal: notaFinal
     });
 
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({ 
+      headless: "new",
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+     });
     const page = await browser.newPage();
     await page.setContent(html);
     const pdfBuffer = await page.pdf({ format: 'A4' });
