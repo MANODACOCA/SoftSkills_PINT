b63e380c7c85785712ba4aed0af93bdbfcd1d7d8
Rafael <rafael.rfs.silva95@gmail.com>
2025-08-08 17:48:21 +0100

    q

--- plataforma-web/web/backend/src/utils/gerarCertificado.js
+++ plataforma-web/web/backend/src/utils/gerarCertificado.js
@@ -220,7 +220,7 @@ function gerarHtmlCertificado({
 
       <div class="assinaturas">
         <div class="assinatura">
-          <div>>DEBUG: ${nomeFormador}</div>
+          <div>DEBUG: ${nomeFormador}</div>
           <hr />
           <div>Formador Responsável</div>
         </div>
