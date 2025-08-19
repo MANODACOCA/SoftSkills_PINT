06e623060ccb58bf46591009ba6b10bcc012deea
Catarina Costa <catarinac.2005@gmail.com>
2025-08-19 18:12:11 +0100

    blah blah blah

--- plataforma-web/web/frontend/src/views/pages/admin/course/Criar_Course.jsx
+++ plataforma-web/web/frontend/src/views/pages/admin/course/Criar_Course.jsx
@@ -129,6 +129,15 @@ const CreateCourse = () => {
             return;
         }
 
+        if (dCursoFim <= dInscFim) {
+            Swal.fire({
+                icon: "error",
+                title: "Datas inválidas",
+                text: "A data de fim da inscrição tem de ser anterior à data de fim do curso.",
+            });
+            return;
+        }
+
         if (cursos.issincrono && dCursoIni < dInscFim) {
             Swal.fire({
                 icon: "error",
