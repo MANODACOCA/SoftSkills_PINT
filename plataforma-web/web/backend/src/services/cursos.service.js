b935d712a4053c5ed46187dafea9b17d9bc0aa36
Catarina Costa <catarinac.2005@gmail.com>
2025-08-19 19:00:41 +0100

    s

--- plataforma-web/web/backend/src/services/cursos.service.js
+++ plataforma-web/web/backend/src/services/cursos.service.js
@@ -796,10 +796,10 @@ async function verifyInscription(userId, cursoId) {
         [Op.or]: [
           {
             data_inicio_curso: {
-              [Op.lte]: Sequelize.literal("DATE(NOW() AT TIME ZONE 'Europe/Lisbon') - 1")
+              [Op.lte]: Sequelize.literal("DATE(NOW() AT TIME ZONE 'Europe/Lisbon') ")
             },
             data_fim_curso: {
-              [Op.gte]: Sequelize.literal("DATE(NOW() AT TIME ZONE 'Europe/Lisbon')")
+              [Op.gte]: Sequelize.literal("DATE(NOW() AT TIME ZONE 'Europe/Lisbon') ")
             }
           },
           // Caso o curso já tenha terminado, mas seja síncrono
