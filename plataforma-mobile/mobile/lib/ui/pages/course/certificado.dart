24e7b587ea14b2d3e340fe37b54341e0da59a634
Catarina Costa <catarinac.2005@gmail.com>
2025-09-04 14:53:49 +0100

    sss

--- plataforma-mobile/mobile/lib/ui/pages/course/certificado.dart
+++ plataforma-mobile/mobile/lib/ui/pages/course/certificado.dart
@@ -1,25 +1,63 @@
-import 'package:go_router/go_router.dart';
 import 'package:mobile/ui/core/shared/base_comp/app_bar_arrow.dart';
-
+import 'package:go_router/go_router.dart';
 import '../../core/shared/export.dart';
+import '../../../API/certificado_api.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class CertificadoPage extends StatelessWidget {
-  const CertificadoPage({super.key, required this.formandoId, required this.cursoId});
-
-  final int formandoId;
+class CertificadoPage extends StatefulWidget {
   final int cursoId;
+  final int formandoId;
+
+  const CertificadoPage({
+    super.key,
+    required this.cursoId,
+    required this.formandoId,
+  });
+
+  @override
+  State<CertificadoPage> createState() => _CertificadoPageState();
+}
+
+class _CertificadoPageState extends State<CertificadoPage> {
+  late final WebViewController _controller;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.white);
+
+    _loadHtml();
+  }
+
+  Future<void> _loadHtml() async {
+    try {
+      final html = await CertificadoApi().geraCertificado(widget.cursoId, widget.formandoId);
+      await _controller.loadHtmlString(html);
+    } catch (e) {
+      if(!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao carregar certificado: $e')),
+      );
+    } finally {
+      if (mounted) setState(() => _loading = false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AppScaffold(
-      appBar: AppBarArrow(
-        title: 'Curso', 
-        onBack: () => context.go('/cursos-completed')
+    return Scaffold(
+      appBar: AppBarArrow(title: 'Certificado', onBack: () => context.go('/cursos-completed')),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (_loading)
+            const Center(child: CircularProgressIndicator()),
+        ],
       ),
-      body: Center(
-        child: Text('Certificado curso'),
-      ),
-      bottomNavigationBar: Footer(),
     );
   }
 }
